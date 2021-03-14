947cdc74a274f4fbbfc43e8c12fb70aaeb7a0ba0
Елизавета <lisabeta777@mail.ru>
2021-03-14 13:00:13 +0300

    ИСПРАВЛЕНИЯ

--- src/com/company/Division.java
+++ src/com/company/Division.java
@@ -1,8 +1,8 @@
 package com.company;
 
-public class Multiplied extends Operation {
+public class Division extends Operation{
 
     public static double execute(double a, int b) {
-        return (a * b);
+        return (a / b);
     }
 }
--- src/com/company/Plus.java
+++ src/com/company/Plus.java
@@ -1,9 +1,4 @@
 package com.company;
-public class Plus extends Operation {
 
-    static void plus(int a, int b) {
-        int sum;
-        sum = a + b;
-        System.out.println(sum);
-    }
+public class Plus extends Operation{
 }
